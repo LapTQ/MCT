89ee778106d8ceff77a37c92e70e20917727670e
ProgrammingForFun <lap.tq4@gmail.com>
2023-07-21 17:26:52 +0700

    msg

--- app/main/routes.py
+++ app/main/routes.py
@@ -46,51 +46,55 @@ def view_staff_list():
     return render_template('view_staff_list.html', users=users)
 
 
-@bp.route('/register_workshift', methods=['GET', 'POST'])
+@bp.route('/register_workshift/<username>', methods=['GET', 'POST'])
 @login_required
-def register_workshift():
+def register_workshift(username):
 
-    if current_user.role not in ['intern', 'engineer']: # type: ignore
+    if current_user.role != 'manager': # type: ignore
         return redirect(url_for('main.index'))
     
     form = RegisterWorkshiftForm()
     if form.validate_on_submit():
 
+        user = User.query.filter_by(username=username).first_or_404()
+
         workshift = RegisteredWorkshift.query.filter_by(
-            user_id=current_user.id, # type: ignore
+            user_id=user.id, # type: ignore
             day=form.day.data,
             dayshift_id=DayShift.query.filter_by(name=form.shift.data).first().id
         ).first()
         
         if workshift is not None:
             flash('Workshift already exists.')
-            return redirect(url_for('main.register_workshift'))
+            return redirect(url_for('main.register_workshift', username=username))
         
         workshift = RegisteredWorkshift(
-            user_id=current_user.id, # type: ignore
+            user_id=user.id, # type: ignore
             day=form.day.data,
             dayshift_id=DayShift.query.filter_by(name=form.shift.data).first().id
         )
         db.session.add(workshift)
         db.session.commit()
-        flash(f'Registered {workshift}')
-        return redirect(url_for('main.user', username=current_user.username))   # type:ignore
+        flash(f'Registered {workshift} for {user}')
+        return redirect(url_for('main.user', username=user.username))   # type:ignore
     
     return render_template('register_workshift.html', form=form)
 
 
-@bp.route('/_unregister_workshift/<day>/<dayshift_id>', methods=['POST'])
+@bp.route('/_unregister_workshift/<username>/<day>/<dayshift_id>', methods=['POST'])
 @login_required
-def unregister_workshift(day, dayshift_id):
+def unregister_workshift(username, day, dayshift_id):
 
-    if current_user.role not in ['intern', 'engineer']: # type: ignore
+    if current_user.role != 'manager': # type: ignore
         return redirect(url_for('main.index'))
     
     form = EmptyForm()
     if form.validate_on_submit():
+
+        user = User.query.filter_by(username=username).first_or_404() 
     
         workshift = RegisteredWorkshift.query.filter_by(
-            user_id=current_user.id,     # type: ignore
+            user_id=user.id,     # type: ignore
             day=day, 
             dayshift_id=dayshift_id
         ).first()
@@ -98,9 +102,9 @@ def unregister_workshift(day, dayshift_id):
         if workshift:
             db.session.delete(workshift)
             db.session.commit()
-            flash(f'Unregisterd day={day}, dayshift_id={dayshift_id}')
+            flash(f'Unregisterd workshift of {user} day={day}, dayshift_id={dayshift_id}')
         
-    return redirect(url_for('main.user', username=current_user.username)) # type: ignore
+    return redirect(url_for('main.user', username=user.username)) # type: ignore
     
 
 @bp.route('/view_weekly_schedule')
